139d3220bb4f38821052f9a902fc280783f4d0da
reda <reda@192.168.1.7>
2018-02-16 23:52:08 +0000

    initial project

---
+++ src/firstProject/Main.java
@@ -0,0 +1,17 @@
+package com.company.test;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner clavier=new Scanner(System.in);
+				int a;
+				int b;
+				String word="bonjour";
+			System.out.print("donner un nombre");
+		a=clavier.nextInt();
+		System.out.print("a= "+a + " ");
+		System.out.print("word : "+word);
+	}
+}
